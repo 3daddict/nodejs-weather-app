b76ea3b5133b07c4539e53b45ece5099c6ad568b
Michael <print@3daddict.com>
2019-05-09 20:24:10 -0700

    basic error handling

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const request = require('request');
 const url = `https://api.darksky.net/forecast/${process.env.WEATHER_SECRET_KEY}/33.7455,117.8677`;
 
 request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.daily.data[0].summary);
+    if(error) {
+        console.log('Unable to connect to weather service')
+    } else if(response.body.error) {
+        console.log('Unable to find location')
+    } else {
+        console.log(response.body.daily.data[0].summary);
+    }
+    
 });
 
 const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/chester.json?proximity=-74.70850,40.78375&access_token=${process.env.MAP_SECRET_KEY}`
