20236c3d4a5980616cfa0d302e04581c1e25896e
Michael <print@3daddict.com>
2019-05-09 20:03:56 -0700

    lat and long request made

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
 const request = require('request');
 
-const url = `https://api.darksky.net/forecast/${process.env.SECRET_KEY}/37.8267,-122.4233`;
+const url = `https://api.darksky.net/forecast/${process.env.WEATHER_SECRET_KEY}/33.7455,117.8677`;
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.daily.data[0].summary);
+});
+
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/chester.json?proximity=-74.70850,40.78375&access_token=${process.env.MAP_SECRET_KEY}`
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[1];
+    const longitute = response.body.features[0].center[0];
+    console.log(latitude, longitute);
 });
