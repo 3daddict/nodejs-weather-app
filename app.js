108ba9725f1f62e5788e075f60889ea2c04d6c7f
Michael <print@3daddict.com>
2019-05-11 09:51:35 -0700

    callback chain of functions

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
-const request = require('request');
 const geocode = require('./utils/geocode');
 const forcast = require('./utils/forcast');
 
+//Geocode finds the lat, long and place name
 geocode('montreal', (error, data) => {
     console.log('Error:', error);
     console.log('Data:', data);
+    //Forcast finds the weather summary
+    //Forcast uses the data object from geocode
+    forcast(data.latitude, data.longitude, (error, data) => {
+        console.log('Error:', error);
+        console.log('Data:', data);
+    });
 });
 
-forcast(33.7455, 117.8677, (error, data) => {
-    console.log('Error:', error);
-    console.log('Data:', data);
-});
