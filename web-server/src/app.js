39e35a58c72880ce3f3780fd8f0f91e3fe34faea
Michael <print@3daddict.com>
2019-05-18 15:48:16 -0700

    help and about html pages created

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,23 +5,6 @@ const app = express();
 //express static route to main dir
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/help', (req, res) => {
-    res.send([
-        {
-            name: 'Mike',
-            age: 36
-        },
-        {
-            name: 'Sarah',
-            age: 26
-        }
-    ])
-});
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-});
-
 app.get('/weather', (req, res) => {
     res.send({
         forcast: 'rainy',
