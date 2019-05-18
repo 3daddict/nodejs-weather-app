46886e4df7e0b3d16946655ef269671af57c2454
Michael <print@3daddict.com>
2019-05-18 15:14:11 -0700

    html and object response

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,19 +4,31 @@ const app = express();
 
 //Express get method
 app.get('', (req, res) => {
-    res.send('Hello World!');
+    res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help Page')
+    res.send([
+        {
+            name: 'Mike',
+            age: 36
+        },
+        {
+            name: 'Sarah',
+            age: 26
+        }
+    ])
 });
 
 app.get('/about', (req, res) => {
-    res.send('Title Page')
+    res.send('<h1>About</h1>')
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Weather Page')
+    res.send({
+        forcast: 'rainy',
+        location: 'Montreal'
+    })
 })
  
 app.listen(3000, () => {
