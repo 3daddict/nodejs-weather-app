470b205175592bcf0c6a406f467c386b68fd2448
Michael <print@3daddict.com>
2019-05-26 14:39:19 -0700

    values rendering on the dom from fetch call

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,22 @@ console.log('Client Side JS File Loaded!');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = search.value;
+    messageOne.textContent = "loading...";
+    messageTwo.textContent = "";
 
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error;
             } else {
-                console.log(data.address);
-                console.log(data.forecast);
+                messageOne.textContent = data.address;
+                messageTwo.textContent = data.forecast;
             }
         });
     });
