320da0184cce1ef327549af9416eba6289062dbe
Michael <print@3daddict.com>
2019-05-27 11:35:53 -0700

    geocode url updated

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const geocode = (address, callback) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?proximity=-74.70850,40.78375&access_token=${process.env.MAP_SECRET_KEY}`
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.MAP_SECRET_KEY}`
 
     request({ url, json: true }, (error, { body }) => {
         if(error) {
