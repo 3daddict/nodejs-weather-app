554af4689a084cd74f7c7a539975a499dda3c0fc
Michael <print@3daddict.com>
2019-05-27 11:30:54 -0700

    console logs removed

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,9 +4,6 @@ const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?proximity=-74.70850,40.78375&access_token=${process.env.MAP_SECRET_KEY}`
 
     request({ url, json: true }, (error, { body }) => {
-        console.log('Key:', process.env.MAP_SECRET_KEY);
-        console.log('Error:', error);
-        console.log('Body:', body);
         if(error) {
             callback('Error: unable to connect to geo location services', undefined);
         } else if(body.features.length === 0) {
